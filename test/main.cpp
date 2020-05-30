5c9404e17146bc3ec534dd2712ab98aee6a38adb
Yuji Ito <llamerada.jp@gmail.com>
2020-05-30 22:52:58 +0900

    run first test
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- test/main.cpp
+++ test/main.cpp
@@ -23,28 +23,24 @@
 // picojsonはコピペ用データ構造を作るために使う
 #include "picojson/picojson.h"
 
-class Connection
-{
-public:
+class Connection {
+ public:
   rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection;
   rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel;
   std::string sdp_type;
   picojson::array ice_array;
 
   // Offer/Answerの作成が成功したら、LocalDescriptionとして設定 & 相手に渡す文字列として表示
-  void onSuccessCSD(webrtc::SessionDescriptionInterface *desc)
-  {
+  void onSuccessCSD(webrtc::SessionDescriptionInterface *desc) {
     peer_connection->SetLocalDescription(ssdo, desc);
 
     std::string sdp;
     desc->ToString(&sdp);
-    std::cout << sdp_type << " SDP:begin" << std::endl
-              << sdp << sdp_type << " SDP:end" << std::endl;
+    std::cout << sdp_type << " SDP:begin" << std::endl << sdp << sdp_type << " SDP:end" << std::endl;
   }
 
   // ICEを取得したら、表示用JSON配列の末尾に追加
-  void onIceCandidate(const webrtc::IceCandidateInterface *candidate)
-  {
+  void onIceCandidate(const webrtc::IceCandidateInterface *candidate) {
     picojson::object ice;
     std::string candidate_str;
     candidate->ToString(&candidate_str);
@@ -54,36 +50,30 @@ public:
     ice_array.push_back(picojson::value(ice));
   }
 
-  class PCO : public webrtc::PeerConnectionObserver
-  {
-  private:
+  class PCO : public webrtc::PeerConnectionObserver {
+   private:
     Connection &parent;
 
-  public:
-    PCO(Connection &parent) : parent(parent)
-    {
+   public:
+    PCO(Connection &parent) : parent(parent) {
     }
 
-    void OnSignalingChange(webrtc::PeerConnectionInterface::SignalingState new_state) override
-    {
+    void OnSignalingChange(webrtc::PeerConnectionInterface::SignalingState new_state) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::SignalingChange(" << new_state << ")" << std::endl;
     };
 
-    void OnAddStream(rtc::scoped_refptr<webrtc::MediaStreamInterface> stream) override
-    {
+    void OnAddStream(rtc::scoped_refptr<webrtc::MediaStreamInterface> stream) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::AddStream" << std::endl;
     };
 
-    void OnRemoveStream(rtc::scoped_refptr<webrtc::MediaStreamInterface> stream) override
-    {
+    void OnRemoveStream(rtc::scoped_refptr<webrtc::MediaStreamInterface> stream) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::RemoveStream" << std::endl;
     };
 
-    void OnDataChannel(rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel) override
-    {
+    void OnDataChannel(rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::DataChannel(" << data_channel << ", " << parent.data_channel.get() << ")"
                 << std::endl;
@@ -92,110 +82,93 @@ public:
       parent.data_channel->RegisterObserver(&parent.dco);
     };
 
-    void OnRenegotiationNeeded() override
-    {
+    void OnRenegotiationNeeded() override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::RenegotiationNeeded" << std::endl;
     };
 
-    void OnIceConnectionChange(webrtc::PeerConnectionInterface::IceConnectionState new_state) override
-    {
+    void OnIceConnectionChange(webrtc::PeerConnectionInterface::IceConnectionState new_state) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::IceConnectionChange(" << new_state << ")" << std::endl;
     };
 
-    void OnIceGatheringChange(webrtc::PeerConnectionInterface::IceGatheringState new_state) override
-    {
+    void OnIceGatheringChange(webrtc::PeerConnectionInterface::IceGatheringState new_state) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::IceGatheringChange(" << new_state << ")" << std::endl;
     };
 
-    void OnIceCandidate(const webrtc::IceCandidateInterface *candidate) override
-    {
+    void OnIceCandidate(const webrtc::IceCandidateInterface *candidate) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "PeerConnectionObserver::IceCandidate" << std::endl;
       parent.onIceCandidate(candidate);
     };
   };
 
-  class DCO : public webrtc::DataChannelObserver
-  {
-  private:
+  class DCO : public webrtc::DataChannelObserver {
+   private:
     Connection &parent;
 
-  public:
-    DCO(Connection &parent) : parent(parent)
-    {
+   public:
+    DCO(Connection &parent) : parent(parent) {
     }
 
     // 接続状況が変化した時に発火する。切断は発火タイミングで値を確認して検知可能
-    void OnStateChange() override
-    {
+    void OnStateChange() override {
       std::cout << std::this_thread::get_id() << ":"
                 << "DataChannelObserver::StateChange" << std::endl;
     };
 
     // メッセージ受信
-    void OnMessage(const webrtc::DataBuffer &buffer) override
-    {
+    void OnMessage(const webrtc::DataBuffer &buffer) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "DataChannelObserver::Message" << std::endl;
       std::cout << std::string(buffer.data.data<char>(), buffer.data.size()) << std::endl;
     };
 
-    void OnBufferedAmountChange(uint64_t previous_amount) override
-    {
+    void OnBufferedAmountChange(uint64_t previous_amount) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "DataChannelObserver::BufferedAmountChange(" << previous_amount << ")" << std::endl;
     };
   };
 
-  class CSDO : public webrtc::CreateSessionDescriptionObserver
-  {
-  private:
+  class CSDO : public webrtc::CreateSessionDescriptionObserver {
+   private:
     Connection &parent;
 
-  public:
-    CSDO(Connection &parent) : parent(parent)
-    {
+   public:
+    CSDO(Connection &parent) : parent(parent) {
     }
 
-    void OnSuccess(webrtc::SessionDescriptionInterface *desc) override
-    {
+    void OnSuccess(webrtc::SessionDescriptionInterface *desc) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "CreateSessionDescriptionObserver::OnSuccess" << std::endl;
       parent.onSuccessCSD(desc);
     };
 
-    void OnFailure(const std::string &error) override
-    {
+    void OnFailure(webrtc::RTCError error) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "CreateSessionDescriptionObserver::OnFailure" << std::endl
-                << error << std::endl;
+                << error.message() << std::endl;
     };
   };
 
-  class SSDO : public webrtc::SetSessionDescriptionObserver
-  {
-  private:
+  class SSDO : public webrtc::SetSessionDescriptionObserver {
+   private:
     Connection &parent;
 
-  public:
-    SSDO(Connection &parent) : parent(parent)
-    {
+   public:
+    SSDO(Connection &parent) : parent(parent) {
     }
 
-    void OnSuccess() override
-    {
+    void OnSuccess() override {
       std::cout << std::this_thread::get_id() << ":"
                 << "SetSessionDescriptionObserver::OnSuccess" << std::endl;
     };
 
-    void OnFailure(const std::string &error) override
-    {
+    void OnFailure(webrtc::RTCError error) override {
       std::cout << std::this_thread::get_id() << ":"
                 << "SetSessionDescriptionObserver::OnFailure" << std::endl
-                << error << std::endl;
+                << error.message() << std::endl;
     };
   };
 
@@ -204,11 +177,11 @@ public:
   rtc::scoped_refptr<CSDO> csdo;
   rtc::scoped_refptr<SSDO> ssdo;
 
-  Connection() : pco(*this),
-                 dco(*this),
-                 csdo(new rtc::RefCountedObject<CSDO>(*this)),
-                 ssdo(new rtc::RefCountedObject<SSDO>(*this))
-  {
+  Connection() :
+      pco(*this),
+      dco(*this),
+      csdo(new rtc::RefCountedObject<CSDO>(*this)),
+      ssdo(new rtc::RefCountedObject<SSDO>(*this)) {
   }
 };
 
@@ -219,8 +192,7 @@ rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> peer_connection_facto
 webrtc::PeerConnectionInterface::RTCConfiguration configuration;
 Connection connection;
 
-void cmd_sdp1()
-{
+void cmd_sdp1() {
   connection.peer_connection =
       peer_connection_factory->CreatePeerConnection(configuration, nullptr, nullptr, &connection.pco);
 
@@ -230,23 +202,20 @@ void cmd_sdp1()
   connection.data_channel = connection.peer_connection->CreateDataChannel("data_channel", &config);
   connection.data_channel->RegisterObserver(&connection.dco);
 
-  if (connection.peer_connection.get() == nullptr)
-  {
+  if (connection.peer_connection.get() == nullptr) {
     peer_connection_factory = nullptr;
     std::cout << "Error on CreatePeerConnection." << std::endl;
     return;
   }
-  connection.sdp_type = "Offer"; // 表示用の文字列、webrtcの動作には関係ない
+  connection.sdp_type = "Offer";  // 表示用の文字列、webrtcの動作には関係ない
   connection.peer_connection->CreateOffer(connection.csdo, webrtc::PeerConnectionInterface::RTCOfferAnswerOptions());
 }
 
-void cmd_sdp2(const std::string &parameter)
-{
+void cmd_sdp2(const std::string &parameter) {
   connection.peer_connection =
       peer_connection_factory->CreatePeerConnection(configuration, nullptr, nullptr, &connection.pco);
 
-  if (connection.peer_connection.get() == nullptr)
-  {
+  if (connection.peer_connection.get() == nullptr) {
     peer_connection_factory = nullptr;
     std::cout << "Error on CreatePeerConnection." << std::endl;
     return;
@@ -254,63 +223,51 @@ void cmd_sdp2(const std::string &parameter)
   webrtc::SdpParseError error;
   webrtc::SessionDescriptionInterface *session_description(
       webrtc::CreateSessionDescription("offer", parameter, &error));
-  if (session_description == nullptr)
-  {
+  if (session_description == nullptr) {
     std::cout << "Error on CreateSessionDescription." << std::endl
               << error.line << std::endl
               << error.description << std::endl;
-    std::cout << "Offer SDP:begin" << std::endl
-              << parameter << std::endl
-              << "Offer SDP:end" << std::endl;
+    std::cout << "Offer SDP:begin" << std::endl << parameter << std::endl << "Offer SDP:end" << std::endl;
   }
   connection.peer_connection->SetRemoteDescription(connection.ssdo, session_description);
 
-  connection.sdp_type = "Answer"; // 表示用の文字列、webrtcの動作には関係ない
+  connection.sdp_type = "Answer";  // 表示用の文字列、webrtcの動作には関係ない
   connection.peer_connection->CreateAnswer(connection.csdo, webrtc::PeerConnectionInterface::RTCOfferAnswerOptions());
 }
 
-void cmd_sdp3(const std::string &parameter)
-{
+void cmd_sdp3(const std::string &parameter) {
   webrtc::SdpParseError error;
   webrtc::SessionDescriptionInterface *session_description(
       webrtc::CreateSessionDescription("answer", parameter, &error));
-  if (session_description == nullptr)
-  {
+  if (session_description == nullptr) {
     std::cout << "Error on CreateSessionDescription." << std::endl
               << error.line << std::endl
               << error.description << std::endl;
-    std::cout << "Answer SDP:begin" << std::endl
-              << parameter << std::endl
-              << "Answer SDP:end" << std::endl;
+    std::cout << "Answer SDP:begin" << std::endl << parameter << std::endl << "Answer SDP:end" << std::endl;
   }
   connection.peer_connection->SetRemoteDescription(connection.ssdo, session_description);
 }
 
-void cmd_ice1()
-{
+void cmd_ice1() {
   std::cout << picojson::value(connection.ice_array).serialize(true) << std::endl;
   connection.ice_array.clear();
 }
 
-void cmd_ice2(const std::string &parameter)
-{
+void cmd_ice2(const std::string &parameter) {
   picojson::value v;
   std::string err = picojson::parse(v, parameter);
-  if (!err.empty())
-  {
+  if (!err.empty()) {
     std::cout << "Error on parse json : " << err << std::endl;
     return;
   }
 
   webrtc::SdpParseError err_sdp;
-  for (auto &ice_it : v.get<picojson::array>())
-  {
-    picojson::object &ice_json = ice_it.get<picojson::object>();
+  for (auto &ice_it : v.get<picojson::array>()) {
+    picojson::object &ice_json         = ice_it.get<picojson::object>();
     webrtc::IceCandidateInterface *ice = CreateIceCandidate(
         ice_json.at("sdpMid").get<std::string>(), static_cast<int>(ice_json.at("sdpMLineIndex").get<double>()),
         ice_json.at("candidate").get<std::string>(), &err_sdp);
-    if (!err_sdp.line.empty() && !err_sdp.description.empty())
-    {
+    if (!err_sdp.line.empty() && !err_sdp.description.empty()) {
       std::cout << "Error on CreateIceCandidate" << std::endl
                 << err_sdp.line << std::endl
                 << err_sdp.description << std::endl;
@@ -320,28 +277,25 @@ void cmd_ice2(const std::string &parameter)
   }
 }
 
-void cmd_send(const std::string &parameter)
-{
+void cmd_send(const std::string &parameter) {
   webrtc::DataBuffer buffer(rtc::CopyOnWriteBuffer(parameter.c_str(), parameter.size()), true);
   std::cout << "Send(" << connection.data_channel->state() << ")" << std::endl;
   connection.data_channel->Send(buffer);
 }
 
-void cmd_quit()
-{
+void cmd_quit() {
   // スレッドを活かしながらCloseしないと、別スレッドからのイベント待ちになり終了できなくなる
   connection.peer_connection->Close();
   connection.peer_connection = nullptr;
-  connection.data_channel = nullptr;
-  peer_connection_factory = nullptr;
+  connection.data_channel    = nullptr;
+  peer_connection_factory    = nullptr;
   // リソースを開放したらスレッドを止めてOK
   network_thread->Stop();
   worker_thread->Stop();
   signaling_thread->Stop();
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   webrtc::field_trial::InitFieldTrialsFromString("");
 
   std::cout << std::this_thread::get_id() << ":"
@@ -361,13 +315,12 @@ int main(int argc, char *argv[])
   signaling_thread = rtc::Thread::Create();
   signaling_thread->Start();
   webrtc::PeerConnectionFactoryDependencies dependencies;
-  dependencies.network_thread = network_thread.get();
-  dependencies.worker_thread = worker_thread.get();
+  dependencies.network_thread   = network_thread.get();
+  dependencies.worker_thread    = worker_thread.get();
   dependencies.signaling_thread = signaling_thread.get();
-  peer_connection_factory = webrtc::CreateModularPeerConnectionFactory(std::move(dependencies));
+  peer_connection_factory       = webrtc::CreateModularPeerConnectionFactory(std::move(dependencies));
 
-  if (peer_connection_factory.get() == nullptr)
-  {
+  if (peer_connection_factory.get() == nullptr) {
     std::cout << "Error on CreateModularPeerConnectionFactory." << std::endl;
     return EXIT_FAILURE;
   }
@@ -377,78 +330,47 @@ int main(int argc, char *argv[])
   std::string parameter;
   bool is_cmd_mode = true;
 
-  while (std::getline(std::cin, line))
-  {
-    if (is_cmd_mode)
-    {
-      if (line == "")
-      {
+  while (std::getline(std::cin, line)) {
+    if (is_cmd_mode) {
+      if (line == "") {
         continue;
-      }
-      else if (line == "sdp1")
-      {
+      } else if (line == "sdp1") {
         cmd_sdp1();
-      }
-      else if (line == "sdp2")
-      {
-        command = "sdp2";
+      } else if (line == "sdp2") {
+        command     = "sdp2";
         is_cmd_mode = false;
-      }
-      else if (line == "sdp3")
-      {
-        command = "sdp3";
+      } else if (line == "sdp3") {
+        command     = "sdp3";
         is_cmd_mode = false;
-      }
-      else if (line == "ice1")
-      {
+      } else if (line == "ice1") {
         cmd_ice1();
-      }
-      else if (line == "ice2")
-      {
-        command = "ice2";
+      } else if (line == "ice2") {
+        command     = "ice2";
         is_cmd_mode = false;
-      }
-      else if (line == "send")
-      {
-        command = "send";
+      } else if (line == "send") {
+        command     = "send";
         is_cmd_mode = false;
-      }
-      else if (line == "quit")
-      {
+      } else if (line == "quit") {
         cmd_quit();
         break;
-      }
-      else
-      {
+      } else {
         std::cout << "?" << line << std::endl;
       }
-    }
-    else
-    {
-      if (line == ";")
-      {
-        if (command == "sdp2")
-        {
+    } else {
+      if (line == ";") {
+        if (command == "sdp2") {
           cmd_sdp2(parameter);
-        }
-        else if (command == "sdp3")
-        {
+        } else if (command == "sdp3") {
           cmd_sdp3(parameter);
-        }
-        else if (command == "ice2")
-        {
+        } else if (command == "ice2") {
           cmd_ice2(parameter);
-        }
-        else if (command == "send")
-        {
+        } else if (command == "send") {
           cmd_send(parameter);
         }
 
-        parameter = "";
+        parameter   = "";
         is_cmd_mode = true;
-      }
-      else
-      {
+      } else {
         parameter += line + "\n";
       }
     }
